633d553569e4d6fd57f60b49e310b70cb898c1bf
MSFTserver <msftserver@gmail.com>
2021-01-23 17:05:53 -0800

    update title

--- src/settings/meta.js
+++ src/settings/meta.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'npm audit fix',
+  title: 'print2a',
   description: '3d printed weaponry from across the globe',
   keywords: '3d print, print2a, print 2a, 3d printing, pewpew, fmda, awcy, det_disp, guns',
   url: 'https://print2a.com',
