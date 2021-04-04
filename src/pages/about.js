351dd825aea827007027bd3f707d9cbd8499a67c
Dook97 <30779172+Dook97@users.noreply.github.com>
2021-04-04 13:28:51 +0000

    fix typos

--- src/pages/about.js
+++ src/pages/about.js
@@ -24,9 +24,9 @@ class About extends React.Component {
       <Main className={classes.root}>
         <div className='aboutPage'>
           <h1><Text>Print2a</Text></h1>
-          <p><Text>This is a collection of 3D Printed weaponry. These files have been collected across various repositories to bring them to one easy to find organized place. some repos include fosscad, AWCY?, det_disp and other sources far and wide!</Text></p>
+          <p><Text>This is a collection of 3D Printed weaponry. These files have been collected across various repositories to bring them to one easy to find organized place. Some repos include fosscad, AWCY?, det_disp and other sources far and wide!</Text></p>
           <h2><Text>Donate</Text></h2>
-          <p><Text>if you like the site and what i have put together, please consider donating to help with server costs to keep this site running, anything helps as costs are quite low right now.</Text></p>
+          <p><Text>If you like the site and what I have put together, please consider donating to help with server costs to keep this site running. Anything helps as costs are quite low right now.</Text></p>
           <div style={{lineHeight:"5px"}}>
           <p><Text>PayPal:</Text></p><p><Link href="http://donate.print2a.com" target="PayPal"><Text>http://donate.print2a.com</Text></Link></p><br style={{lineHeight:'20px'}}></br>
           <p><Text>BTC Address:</Text></p><p><Text>34CnFqMvsrmhZA1kDyV6iD91GV9Vn3dd94</Text></p><br style={{lineHeight:'20px'}}></br>
@@ -37,7 +37,7 @@ class About extends React.Component {
           </div>
           <Fader>
           <h2><Text>Github?</Text></h2>
-          <p><Text>this repo has been removed from github due to issues with the large files and my ever increasing bandwidth from you CHADS cloning the repo, due to some GitHub oversights everytime some clones or downloads this repo it adds to my bandwidth, additionally everytime the cloned repo is changed on the user end it adds to the Main repo owners bandwidth and storage wich was hell to find out so alas this repo is gone from github.</Text></p>
+          <p><Text>This repo has been removed from github due to issues with the large files and my ever increasing bandwidth from you CHADS cloning the repo. Due to some GitHub oversights everytime someone clones or downloads this repo it adds to my bandwidth, additionally everytime the cloned repo is changed on the user end it adds to the Main repo owner's bandwidth and storage wich was hell to find out so alas this repo is gone from github.</Text></p>
           </Fader>
           <h2><Text>License</Text></h2>
           <li> These files are released open source.</li>
