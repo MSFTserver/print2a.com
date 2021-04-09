06de5f45de506199fc3b75c2ad9267fdd4e11693
MSFTserver <msftserver@gmail.com>
2021-04-09 11:25:04 -0700

    Update latest.js

--- static/latest.js
+++ static/latest.js
@@ -5,14 +5,13 @@ const print2aApiEndpoint = `${print2aApiHost}:${print2aApiPort}`;
 const GetLatest = async () => {
   let response = await fetch(`${print2aApiEndpoint}/LatestProjects`);
   response = await response.json;
-  return {
-    response.map((file, index) => (
+  return response.map((file, index) => (
     <Post
       key={index}
       audio={{ silent: index > 4 }}
       data={{ ...file, id: "file" + index }}
     />
-  ))}
+  ))
 };
 
 export default GetLatest;
