ff2f2ab1a79f7dbe97d0164e09c4345c5162f715
MSFTserver <msftserver@gmail.com>
2021-04-09 11:46:05 -0700

    Update latest.js

--- static/latest.js
+++ static/latest.js
@@ -4,17 +4,19 @@ const print2aApiHost = "https://print2a.com";
 const print2aApiPort = "5757";
 const print2aApiEndpoint = `${print2aApiHost}:${print2aApiPort}`;
 
-const GetLatest = async () => {
+const GetLatest = () => {
   if (typeof window !== "undefined"){
-    let response = await fetch(`${print2aApiEndpoint}/LatestProjects`);
-    response = await response.json;
-    return response.map((file, index) => (
-      <Post
-        key={index}
-        audio={{ silent: index > 4 }}
-        data={{ ...file, id: "file" + index }}
-      />
-    ))
+    let response = fetch(`${print2aApiEndpoint}/LatestProjects`).then(response => {
+      return response.json();
+    }).then(response => {
+      return response.map((file, index) => (
+        <Post
+          key={index}
+          audio={{ silent: index > 4 }}
+          data={{ ...file, id: "file" + index }}
+        />
+      ))
+    })
   }
 };
 
