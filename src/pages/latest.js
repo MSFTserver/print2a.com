b1edc519576026092bf7a211e3e199bf5b6d10a6
MSFTserver <msftserver@gmail.com>
2021-04-09 13:35:02 -0700

    try and fix latest

--- src/pages/latest.js
+++ src/pages/latest.js
@@ -7,30 +7,16 @@ import { Main } from '../components/Main';
 import { Post } from '../components/Post';
 import { Secuence } from '../components/Secuence';
 import { Text } from '../components/Text';
+import GetLatest from '/static/latest.js';
 
 const styles = theme => ({
   root: {}
 });
-const print2aApiHost = "https://print2a.com";
-const print2aApiPort = "5757";
-const print2aApiEndpoint = `${print2aApiHost}:${print2aApiPort}`;
 
 class Latest extends React.Component {
   static propTypes = {
     classes: PropTypes.object
   };
-
-  const [latest, setLatest] = useState([{title: "LOADING...", tags: "LOADING...", link: "#"}]);
-  useEffect(() => {
-    if (typeof window !== "undefined"){
-      async function getLatest(){
-        const req = await fetch(`${print2aApiEndpoint}/LatestProjects`);
-        const res = await response.json();
-        setProjects(res);
-      }
-      getLatest()
-    }
-  }, [])
 
   render () {
     const { classes } = this.props;
@@ -40,14 +26,7 @@ class Latest extends React.Component {
           <header>
             <h1><Text>Latest Files</Text></h1>
           </header>
-          {
-            latest.map((file, index) => (
-            <Post
-              key={index}
-              audio={{ silent: index > 4 }}
-              data={{ ...file, id: 'file' + index }}
-            />
-          ))}
+          <GetLatest />
         </Secuence>
       </Main>
     )
--- static/latest.js
+++ static/latest.js
@@ -1,23 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import { Post } from '../components/Post';
 
 const print2aApiHost = "https://print2a.com";
 const print2aApiPort = "5757";
 const print2aApiEndpoint = `${print2aApiHost}:${print2aApiPort}`;
 
 const GetLatest = () => {
-  if (typeof window !== "undefined"){
-    let response = fetch(`${print2aApiEndpoint}/LatestProjects`).then(response => {
-      return response.json();
-    }).then(response => {
-      return response.map((file, index) => (
-        <Post
-          key={index}
-          audio={{ silent: index > 4 }}
-          data={{ ...file, id: "file" + index }}
-        />
-      ))
-    })
-  }
+  const [latest, setLatest] = useState([{title: "LOADING...", tags: "LOADING...", link: "#"}]);
+  useEffect(() => {
+    if (typeof window !== "undefined"){
+      async function getLatest(){
+        const req = await fetch(`${print2aApiEndpoint}/LatestProjects`);
+        const res = await response.json();
+        setProjects(res);
+      }
+      getLatest()
+    }
+  }, [])
+  return (
+    latest.map((file, index) => (
+    <Post
+      key={index}
+      audio={{ silent: index > 4 }}
+      data={{ ...file, id: 'file' + index }}
+    />
+    ))
+  )
 };
-
 export default GetLatest;
